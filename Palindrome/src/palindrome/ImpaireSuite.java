98a1c85207ccaac25ca295889b042ca7724950b4
Dr.-Ing. Franklin Tchakounté <tchafros@gmail.com>
2017-03-29 17:27:51 +0100

    Bon!

--- Palindrome/src/palindrome/ImpaireSuite.java
+++ Palindrome/src/palindrome/ImpaireSuite.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Ecrire la description du problème ici
  */
 package palindrome;
 
